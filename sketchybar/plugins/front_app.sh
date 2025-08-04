c469be796bf384015f15fb006bb03665ada1d03f
Linyun He <20322006+he-linyun@users.noreply.github.com>
2025-08-03 23:33:03 -0400

    Update sketchybar config
    1. Change the colors.sh with multiple pre-set color themes
    2. Update aerospace config

--- sketchybar/plugins/front_app.sh
+++ sketchybar/plugins/front_app.sh
@@ -1,10 +1,19 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Some events send additional information specific to the event in the $INFO
 # variable. E.g. the front_app_switched event sends the name of the newly
 # focused application in the $INFO variable:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
+PLUGIN_DIR="$CONFIG_DIR/plugins"
+font_app_item=(
+  icon="$($PLUGIN_DIR/icon_map.sh "$INFO")"
+  icon.font="sketchybar-app-font:Regular:16.0"
+  icon.drawing=on
+  label="$INFO"
+)
+
+
 if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set "$NAME" label="$INFO"
+  sketchybar --set "$NAME" "${font_app_item[@]}"
 fi

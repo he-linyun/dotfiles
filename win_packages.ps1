ca9dc0ffbb813d4d67b1d38c756b1c0aefc517da
Linyun He <20322006+he-linyun@users.noreply.github.com>
2026-04-01 15:28:30 -0400

    [win]: Change winget package format from json to plain ps shell
    Easier to look at the packages no longer needed (comment out), without
    searching the git history

---
+++ win_packages.ps1
@@ -0,0 +1,40 @@
+$packages = @(
+    "7zip.7zip",
+    "astral-sh.uv",
+    "CrystalDewWorld.CrystalDiskInfo",
+    "DigitalScholar.Zotero",
+    "Docker.DockerDesktop",
+    "Fastfetch-cli.Fastfetch",
+    "Git.Git",
+    "GitHub.GitHubDesktop",
+    "Google.Antigravity",
+    "Google.Chrome",
+    "Guru3D.Afterburner",
+    "JesseDuffield.lazygit",
+    "Klocman.BulkCrapUninstaller",
+    "Logitech.OptionsPlus",
+    "Microsoft.AppInstaller",
+    "Microsoft.PowerToys",
+    "Microsoft.VisualStudioCode",
+    "Microsoft.WindowsTerminal",
+    "Microsoft.WSL",
+    "Neovim.Neovim",
+    "NetEase.UURemote",
+    "Obsidian.Obsidian",
+    "REALiX.HWiNFO",
+    "Syncthing.Syncthing",
+    "Tailscale.Tailscale",
+    "TechPowerUp.GPU-Z",
+    "Valve.Steam",
+    "Zen-Team.Zen-Browser",
+    # "Zettlr.Zettlr",
+)
+
+Write-Host "Starting WinGet package installation..." -ForegroundColor Cyan
+
+foreach ($package in $packages) {
+    Write-Host "Installing $package..." -ForegroundColor Yellow
+    winget install --id $package -e -s winget --accept-package-agreements --accept-source-agreements --silent
+}
+
+Write-Host "All packages processed!" -ForegroundColor Green
